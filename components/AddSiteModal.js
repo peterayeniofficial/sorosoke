80011ba6fbf2f434045c61d108c07075f4ef3e15
Peter Ayeni <ayenicology@gmail.com>
2021-05-11 21:21:45 +0100

    feat: add site form

---
+++ components/AddSiteModal.js
@@ -0,0 +1,97 @@
+import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Button,
+  Input,
+  useDisclosure,
+  useToast
+} from '@chakra-ui/react';
+
+import { createSite } from '@/lib/db';
+import { useAuth } from '@/lib/auth';
+
+const AddSiteModal = () => {
+  const initialRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { handleSubmit, register } = useForm();
+  const toast = useToast();
+  const auth = useAuth();
+
+  const onCreateSite = ({ site, url }) => {
+    createSite({
+      authorId: auth.user.uid,
+      createdAt: new Date().toISOString(),
+      site,
+      url
+    });
+    toast({
+      title: 'Success!',
+      description: "We've added your site.",
+      status: 'success',
+      duration: 5000,
+      isClosable: true
+    });
+    onClose();
+  };
+
+  return (
+    <>
+      <Button fontWeight="medium" maxW="200px" onClick={onOpen}>
+        Add Your First Site
+      </Button>
+      <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent as="form" onSubmit={handleSubmit(onCreateSite)}>
+          <ModalHeader fontWeight="bold">Add Site</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+              <Input
+                ref={initialRef}
+                placeholder="My site"
+                {...register('Site', {
+                  required: true
+                })}
+              />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Link</FormLabel>
+              <Input
+                placeholder="https://website.com"
+                name="url"
+                {...register('url', { required: true })}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose} mr={3} fontWeight="medium">
+              Cancel
+            </Button>
+            <Button
+              backgroundColor="#99FFFE"
+              color="#194D4C"
+              fontWeight="medium"
+              type="submit"
+            >
+              Create
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AddSiteModal;
