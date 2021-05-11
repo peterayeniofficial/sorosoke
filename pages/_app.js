fb6704933e022d944dca592342dd23509ed5aa8d
Peter Ayeni <ayenicology@gmail.com>
2021-05-11 18:17:49 +0100

    add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,50 @@
-import '../styles/globals.css';
-import { AuthProvider } from '../lib/auth';
+import { ChakraProvider, extendTheme, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+
+import { AuthProvider } from '@/lib/auth';
+
+const theme = extendTheme({
+  fonts: {
+    body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    bold: 800
+  }
+});
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 300px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
